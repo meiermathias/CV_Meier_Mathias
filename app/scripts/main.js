b80dfa094bb1786ffaab6242352060e1952276b6
Mathias Meier <meiermathi@MacBook-Pro-de-Mathias.local>
2016-06-22 01:18:16 +0200

    Ticket-10 : Add music scroll + profil image
    - Add a new image
    - Add a new music
    - Launch the music « panda.mp3 » when you click on the « home » button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,13 @@ $(document).ready(function(){
   'use strict';
   $('a').smoothScroll({speed:10000});
 });
+
+// Elevator script included on the page, already.
+window.onload = function() {
+  var elevator = new Elevator({
+    element: document.querySelector('.elevator-button'),
+    targetElement: document.querySelector('#elevator-target'),
+    mainAudio: '/music/panda.mp3',
+    endAudio: '/music/panda.mp3'
+  });
+}
