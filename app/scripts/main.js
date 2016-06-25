5159c303091f1f482b7ee67ac8ca43770034e30f
meiermathias <mathiasmeier.11@gmail.com>
2016-06-25 02:31:21 +0200

    delete donut/prog

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,30 +12,3 @@ window.onload = function() {
     endAudio: '/images/panda.mp3'
   });
 }
-
-/*
-$('div.progress').each(function(i, obj){
-
-  var label = $(obj).find('div').text();
-  var value = $(obj).find('div').attr('aria-valuenow');
-  var replaceProgessDonut = $('<canvas width="400" height="400"></canvas>');
-  $(obj).replaceWith(replaceProgessDonut);
-var myChart = new Chart(replaceProgessDonut, {
-      type: 'doughnut',
-      data: {
-          labels: [label],
-          datasets: [{
-              data: [value, 100-value],
-              backgroundColor: [
-                  'rgba(153, 102, 255, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(153, 102, 255, 1)'
-              ],
-              borderWidth: 1
-          }]
-      }
-  });
-
-});
-  */
